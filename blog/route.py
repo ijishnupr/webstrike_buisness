61049cd30d8fd69c58b4b1680fd7399017b3f902
ijishnupr <ijishnupr@gmail.com>
2025-12-04 22:40:51 +0530

    done

--- blog/route.py
+++ blog/route.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from blog.service import get_blogs
-from fastapi import Depends
+from fastapi import Depends, APIRouter
 from shared.db import get_connection
-route = APIRoute()
+route = APIRouter()
 
 @route.post("/category")
 async def create_category_route():
     pass
 
-@router.get("/blog")
+@route.get("/blog")
 async def get_blogs_route(db=Depends(get_connection),category:str|None=None,date:datetime|None=None ):
     return await get_blogs(category,date, db)
